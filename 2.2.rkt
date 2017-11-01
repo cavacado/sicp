c4aef0d0d40e8a24b05016346c2e9e30fc865907
cavacado <ngzhaoloong@gmail.com>
2017-11-01 14:49:51 +0800

    done ex 2.19

--- 2.2.rkt
+++ 2.2.rkt
@@ -42,7 +42,8 @@
 (define (reverse-new list)
   (if (null? (cdr list))
       list
-      (append (reverse-new (cdr list)) (cons (car list) nil))))
+      (append (reverse-new (cdr list))
+              (cons (car list) nil))))
 
 ;(reverse-new (list 1 2 3))
 ;(append (reverse-new (list 2 3)) (list 1))
@@ -50,3 +51,33 @@
 ;(append (append (list 3)  (list 2)) (list 1))
 ;(append (list 3 2) (list 1))
 ;(list 3 2 1)
+
+; ex 2.19
+
+(define us-coins
+  (list 50 25 10 5 1))
+
+(define uk-coins
+  (list 100 50 20 10 5 2 1 0.5))
+
+(define (cc amount coin-values)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0)
+             (no-more? coin-values))
+         0)
+        (else
+         (+ (cc amount (except-first-denomination coin-values))
+            (cc (- amount (first-denomination coin-values))
+                coin-values)))))
+
+(define first-denomination car)
+
+(define except-first-denomination cdr)
+
+(define no-more? null?)
+
+; the order of the list does not matter
+; the value will cancel out eventually
+; since it computes all possible combintations
+
+; ex 2.20
