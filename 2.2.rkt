d140cd92d76a682b7ff685dd7d122a4df2922172
cavacado <ngzhaoloong@gmail.com>
2017-11-02 16:24:46 +0800

    done ex 2.34

--- 2.2.rkt
+++ 2.2.rkt
@@ -448,3 +448,11 @@
                 (+ 1 y))
               0
               sequence))
+
+; ex 2.34
+
+(define (horner-eval x coefficient-sequence)
+  (/ (accumulate (lambda (this-coeff higher-terms)
+                (* (+ this-coeff higher-terms) x))
+              0
+              coefficient-sequence) x))
