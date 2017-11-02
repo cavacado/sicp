f1fdb629bb78762d37594e2584a268d746484125
cavacado <ngzhaoloong@gmail.com>
2017-11-02 15:11:13 +0800

    done ex 2.33

--- 2.2.rkt
+++ 2.2.rkt
@@ -357,3 +357,94 @@
         (list 1 3)
         (list 1 2)
         (list 1 2 3)))
+
+(define (filter predicate sequence)
+  (cond ((null? sequence) nil)
+        ((predicate (car sequence))
+         (cons (car sequence)
+               (filter predicate
+                       (cdr sequence))))
+        (else (filter predicate
+                      (cdr sequence)))))
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op
+                      initial
+                      (cdr sequence)))))
+
+(define (enumerate-interval low high)
+  (if (> low high)
+      nil
+      (cons low
+            (enumerate-interval (+ low 1)
+                                high))))
+
+(define (enumerate-tree tree)
+  (cond ((null? tree) nil)
+        ((pair? tree) (append (enumerate-tree (car tree))
+                              (enumerate-tree (cdr tree))))
+        (else (list tree))))
+
+(define (sum-odd-squares tree)
+  (accumulate +
+              0
+              (map square
+                   (filter odd?
+                           (enumerate-tree tree)))))
+
+(define (fib n)
+  (fib-iter 1 0 n))
+
+(define (fib-iter a b count)
+  (if (= count 0)
+      b
+      (fib-iter (+ a b) a (- count 1))))
+
+(define (even-fibs n)
+  (accumulate cons
+              nil
+              (filter even?
+                      (map fib
+                           (enumerate-interval 0 n)))))
+
+(define (list-fib-squares n)
+  (accumulate cons
+              nil
+              (map square
+                   (map fib
+                        (enumerate-interval 0 n)))))
+
+(define (product-of-squares-of-odd-elements sequence)
+  (accumulate *
+              1
+              (map square
+                   (filter odd?
+                           sequence))))
+
+;(define (salary-of-highest-paid-programmer records)
+;  (accumulate max
+;              0
+;              (map salary
+;                   (filter programmer? records))))
+
+; ex 2.33
+(define (map-acc p sequence)
+  (accumulate (lambda (x y)
+                (cons (p x)
+                      y))
+              nil
+              sequence))
+
+(define (append-acc seq1 seq2)
+  (accumulate cons
+              seq2
+              seq1))
+
+(define (length-acc sequence)
+  (accumulate (lambda (x y)
+                (+ 1 y))
+              0
+              sequence))
