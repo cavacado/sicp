a4e2d5de557002bcc13cfe706cec0870e3322855
cavacado <ngzhaoloong@gmail.com>
2017-11-03 10:24:27 +0800

    done ex 2.38

--- 2.2.rkt
+++ 2.2.rkt
@@ -523,3 +523,20 @@
          m)))
 
 ; ex 2.38
+
+(define (fold-left op initial sequence)
+  (define (iter result rest)
+    (if (null? rest)
+        result
+        (iter (op result (car rest))
+              (cdr rest))))
+  (iter initial sequence))
+
+; (fold-right / 1 (list 1 2 3)) => (/ 1 (/ 2 (/ 3 1))) => 1.5
+; (fold-left / 1 (list 1 2 3)) => (/ (/ (/ 1 1) 2) 3) => 1/6
+; (fold-right list nil (list 1 2 3)) => (list 1 (list 2 (list 3 nil)))
+; (fold-left list nil (list 1 2 3)) => (list (list (list nil 1) 2) 3)
+
+; op should be direction independent, so something like + or * should be fine
+
+; ex 2.39
