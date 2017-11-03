ec737100e46c364cab52bc8c6bb8a20df4c9b1fe
cavacado <ngzhaoloong@gmail.com>
2017-11-03 11:36:19 +0800

    done ex 2.41

--- 2.2.rkt
+++ 2.2.rkt
@@ -560,3 +560,97 @@
                (append (list y) x))
              nil
              sequence))
+
+(define (generate-seq-of-pairs n)
+  (accumulate append
+              nil
+              (map (lambda (i)
+                     (map (lambda (j)
+                            (list i j))
+                          (enumerate-interval 1 (- i 1))))
+                   (enumerate-interval 1 n))))
+
+(define (flatmap proc seq)
+  (accumulate append nil (map proc seq)))
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n
+                            (next test-divisor)))))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (next x)
+  (cond ((= x 2) 3)
+        (else (+ x 2))))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+(define (prime-sum? pair)
+  (prime? (+ (car pair)
+             (cadr pair))))
+
+(define (make-pair-sum pair)
+  (list (car pair)
+        (cadr pair)
+        (+ (car pair) (cadr pair))))
+
+(define (prime-sum-pairs n)
+  (map make-pair-sum
+       (filter prime-sum?
+               (flatmap (lambda (i)
+                          (map (lambda (j)
+                                 (list i j))
+                               (enumerate-interval 1 (- i 1))))
+                        (enumerate-interval 1 n)))))
+
+(define (permutations s)
+  (if (null? s)
+      (list nil)
+      (flatmap (lambda (x)
+                 (map (lambda (p)
+                        (cons x p))
+                      (permutations (remove x s))))
+               s)))
+
+(define (remove item sequence)
+  (filter (lambda (x) (not (= x item)))
+          sequence))
+
+; ex 2.40
+
+(define (unique-pairs n)
+  (flatmap (lambda (i)
+             (map (lambda (j)
+                    (list i j))
+                  (enumerate-interval 1 (- i 1))))
+           (enumerate-interval 1 n)))
+
+(define (prime-sum-pairs-refactor n)
+  (map make-pair-sum
+       (filter prime-sum?
+               (unique-pairs n))))
+
+; ex 2.41
+
+(define (unique-triples n)
+  (flatmap (lambda (k)
+             (flatmap (lambda (j)
+                        (map (lambda (i)
+                               (list i j k))
+                             (enumerate-interval 1 (- j 1))))
+                      (enumerate-interval 1 (- k 1))))
+           (enumerate-interval 1 n)))
+
+(define (ordered-triples n s)
+  (filter (lambda (triple)
+            (= (accumulate + 0 triple) s))
+          (unique-triples n)))
+
+; ex 2.42
