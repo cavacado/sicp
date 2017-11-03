dd034dff61bee7dbc2c3d5a3492b746ddc0c83f6
cavacado <ngzhaoloong@gmail.com>
2017-11-03 16:03:17 +0800

    done ex 2.45 interesting!

--- 2.2.rkt
+++ 2.2.rkt
@@ -1,4 +1,5 @@
 #lang sicp
+(#%require sicp-pict)
 
 (define one-through-four (list 1 2 3 4))
 
@@ -698,3 +699,89 @@
 ; we're transforming a linear recursive process to a tree-recursive
 ; process. since a tree-recursive process grows exponentially,
 ; the time take should be T^ board-size.
+
+(define eins einstein)
+
+(define wave2 (beside eins (flip-vert eins)))
+(define wave4 (below wave2 wave2))
+
+(define (flipped-pairs painter)
+  (let ((painter2
+         (beside painter
+                 (flip-vert painter))))
+    (below painter2 painter2)))
+
+(define wave4-new (flipped-pairs eins))
+
+(define (right-split painter n)
+  (if (= n 0)
+      painter
+      (let ((smaller (right-split painter
+                                  (- n 1))))
+        (beside painter
+                (below smaller smaller)))))
+
+(define (corner-split painter n)
+  (if (= n 0)
+      painter
+      (let ((up (up-split painter (- n 1)))
+            (right (right-split painter (- n 1))))
+        (let ((top-left (beside up up))
+              (bottom-right (below right right))
+              (corner (corner-split painter (- n 1))))
+          (beside (below painter top-left)
+                  (below bottom-right corner))))))
+
+(define (square-limit painter n)
+  (let ((quarter (corner-split painter n)))
+    (let ((half (beside (flip-horiz quarter)
+                        quarter)))
+      (below (flip-vert half) half))))
+
+; ex 2.44
+
+(define (up-split painter n)
+  (if (= n 0)
+      painter
+      (let ((smaller (up-split painter
+                               (- n 1))))
+        (below painter
+               (beside smaller smaller)))))
+
+(define (square-of-four tl tr bl br)
+  (lambda (painter)
+    (let ((top (beside (tl painter)
+                       (tr painter)))
+          (bottom (beside (bl painter)
+                          (br painter))))
+      (below bottom top))))
+
+(define (flipped-pairs-new painter)
+  (let ((combine4
+         (square-of-four identity
+                         flip-vert
+                         identity
+                         flip-vert)))
+    combine4 painter))
+
+(define (square-limit-new painter n)
+  (let ((combine4
+         (square-of-four flip-horiz
+                         identity
+                         rotate180
+                         flip-vert)))
+    (combine4 (corner-split painter n))))
+
+; ex 2.45
+
+(define (split proc1 proc2)
+  (lambda (painter n)
+    (if (= n 0)
+        painter
+        (let ((smaller ((split proc1 proc2) painter
+                                            (- n 1))))
+          (proc1 painter
+                 (proc2 smaller smaller))))))
+
+(define right-split-new (split beside below))
+(define up-split-new (split below beside))
