3ca7267098d866228258dce3a67c193d6dd19faa
cavacado <ngzhaoloong@gmail.com>
2017-11-07 17:25:23 +0800

    done ex 2.63

--- 2.3.rkt
+++ 2.3.rkt
@@ -280,3 +280,72 @@
                                                         set2)))
                  ((< x2 x1) (cons x2 (union-set-ordered set1
                                                         (cdr set2)))))))))
+
+(define (entry tree)
+  (car tree))
+
+(define (left-branch tree)
+  (cadr tree))
+
+(define (right-branch tree)
+  (caddr tree))
+
+(define (make-tree entry left right)
+  (list entry left right))
+
+(define (element-of-set-tree? x set)
+  (cond ((null? set) false)
+        ((= x (entry set)) true)
+        ((< x (entry set)) (element-of-set-tree? x (left-branch set)))
+        ((> x (entry set)) (element-of-set-tree? x (right-branch set)))))
+
+(define (adjoin-set-tree x set)
+  (cond ((null? set) (make-tree x '() '()))
+        ((= x (entry set)) set)
+        ((< x (entry set)) (make-tree (entry set)
+                                      (adjoin-set x (left-branch set))
+                                      (right-branch set)))
+        ((> x (entry set)) (make-tree (entry set)
+                                      (left-branch set)
+                                      (adjoin-set x (right-branch set))))))
+
+; ex 2.63
+
+(define (tree->list-1 tree)
+  (cond ((null? tree) '())
+        (else (append (tree->list-1 (left-branch tree))
+                      (cons (entry tree)
+                            (tree->list-1 (right-branch tree)))))))
+
+(define (tree->list-2 tree)
+  (define (copy-to-list tree result-list)
+    (if (null? tree)
+        result-list
+        (copy-to-list (left-branch tree)
+                      (cons (entry tree)
+                            (copy-to-list (right-branch tree)
+                                          result-list)))))
+  (copy-to-list tree '()))
+
+(define tree1 '(7 (3 (1 () ()) (5 () ())) (9 () (11 () ()))))
+(define tree2 '(3 (1 () ()) (7 (5 () ()) (9 () (11 () ())))))
+(define tree3 '(5 (3 (1 () ()) ()) (9 (7 () ()) (11 () ()))))
+
+;(tree->list-1 tree1)
+;(tree->list-1 tree2)
+;(tree->list-1 tree3)
+;(tree->list-2 tree1)
+;(tree->list-2 tree2)
+;(tree->list-2 tree3)
+
+; yes it produces the same result
+;(mcons 1 (mcons 3 (mcons 5 (mcons 7 (mcons 9 (mcons 11 '()))))))
+;(mcons 1 (mcons 3 (mcons 5 (mcons 7 (mcons 9 (mcons 11 '()))))))
+;(mcons 1 (mcons 3 (mcons 5 (mcons 7 (mcons 9 (mcons 11 '()))))))
+;(mcons 1 (mcons 3 (mcons 5 (mcons 7 (mcons 9 (mcons 11 '()))))))
+;(mcons 1 (mcons 3 (mcons 5 (mcons 7 (mcons 9 (mcons 11 '()))))))
+;(mcons 1 (mcons 3 (mcons 5 (mcons 7 (mcons 9 (mcons 11 '()))))))
+
+; if its a balanced tree, then both grow at the same speed.
+
+; ex 2.64
